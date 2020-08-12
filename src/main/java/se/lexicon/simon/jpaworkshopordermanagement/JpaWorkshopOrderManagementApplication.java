18c839536efa87d2fb64c808e56247b9346012ab
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-08-12 09:41:18 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/simon/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.jpaworkshopordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaWorkshopOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/simon/jpaworkshopordermanagement/JpaWorkshopOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.jpaworkshopordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
