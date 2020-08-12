f27e0e32c4305adeb84cb1920fb4ce23d3239c53
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-08-12 10:31:54 +0200

    Product.java Created NOT tested yet.
    - Saving time in lecture.

---
+++ src/main/java/se/lexicon/simon/jpaworkshopordermanagement/entity/Product.java
@@ -0,0 +1,64 @@
+package se.lexicon.simon.jpaworkshopordermanagement.entity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Double.compare(product.price, price) == 0 &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
